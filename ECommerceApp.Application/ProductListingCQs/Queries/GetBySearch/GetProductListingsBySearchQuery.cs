b445790d83e6b6216cd1a6f6e7af727710467ecc
Andrew Richerson <ajorigami1@gmail.com>
2020-11-23 17:33:56 -0600

    Added ProductListing.GetProductsBySearchQuery.

--- ECommerceApp.Application/ProductListingCQs/Queries/GetBySearch/GetProductListingsBySearchQuery.cs
+++ ECommerceApp.Application/ProductListingCQs/Queries/GetBySearch/GetProductListingsBySearchQuery.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
 using ECommerceApp.Application.Common.Interfaces;
 using ECommerceApp.Application.Common.Requests;
+using ECommerceApp.Domain.Common.Mapping;
 using ECommerceApp.Domain.Common.Models;
+using ECommerceApp.Domain.Entities;
 using ECommerceApp.Domain.Interfaces;
 using ECommerceApp.Domain.Models;
 using MediatR;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -32,9 +35,19 @@ namespace ECommerceApp.Application.ProductListingCQs.Queries.GetBySearch
 		{
 		}
 
-		public override Task<PaginatedList<ProductListingQueryDto>> Handle(GetProductListingsBySearchQuery request, CancellationToken cancellationToken)
+		public override async Task<PaginatedList<ProductListingQueryDto>> Handle(GetProductListingsBySearchQuery request, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			var productListings = await UnitOfWork.ProductListings.GetEntitiesAsync();
+			var paginatedListingsBySearch = await productListings.Where(pl => 
+				pl.Brand.Contains(request.SearchTerm) ||
+				pl.Description.Contains(request.SearchTerm) ||
+				pl.Name.Contains(request.SearchTerm)
+			)
+				.ToPaginatedListAsync(request.PagingParams);
+
+			var paginatedDtoResponse = await paginatedListingsBySearch.ToMappedPaginatedListAsync<ProductListing, ProductListingQueryDto>(Mapper);
+
+			return paginatedDtoResponse;
 		}
 	}
 }
